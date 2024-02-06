6c4ae8b772781ce6f6ae8feef6ca758e343abc5c
Aleksandr Khan <manchig1990@gmail.com>
2024-02-06 21:04:00 +1000

    complete

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int count = 0, distance = 10000,
+    firstFriendSpeed = 1, secondFriendSpeed = 2,
+    dogSpeed = 5, friend = 2, time = 0;
+
+while (distance > 10)
+{
+    if (friend == 1)
+    {
+        time = distance / (firstFriendSpeed + dogSpeed);
+        friend = 2;
+    }
+    else
+    {
+        time = distance / (secondFriendSpeed + dogSpeed);
+        friend = 1;
+    }
+    distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+    count++;
+}
+Console.Write(count);
